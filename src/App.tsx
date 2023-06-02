e48dbc38c9fce6334459bd6057e60473d04a6c7e
OblivionNoirV2 <donahueb85@gmail.com>
2023-06-02 18:43:00 -0500

    initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import './sparkles.css';
 import './App.css';
-
+const TopBar = () => {
+  return (
+    <section className='crystal items-center w-full h-20 flex justify-end'>
+      <i>
+        <h1 className='mr-8 text-4xl text-white'>
+          Crystal Keys
+        </h1>
+      </i>
+    </section>
+  )
+}
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <main>
+      <TopBar />
+    </main>
   );
 }
 
