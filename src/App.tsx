a0ca5078f1e8e7652e34950ab491b16e1eff2c65
OblivionNoirV2 <donahueb85@gmail.com>
2023-06-23 15:56:53 -0500

    scaling

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ import { KeyTypeContext } from './context';
 
 const TopBar = () => {
   return (
-    <section className='crystal items-center w-full h-20 flex justify-end'>
+    <section className='crystal items-center w-full h-20 flex
+     justify-end'>
       <i>
         <h1 className='mr-8 text-4xl text-white'>
           Crystal Keys
@@ -28,13 +29,14 @@ which then changes the color of whatever key is clicked*/
 //split the keyboard into different segments
 //every element here has its own index, one huge array
 const key_sets: { [row: string]: string[] } = {
+
   top_row: [
     'Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6',
     'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'PrtSc', 'ScrLk', 'Pause',
   ],
   second_row: [
     '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=',
-    'Backspace', 'Ins', 'Home', 'PgUp'
+    'Back', 'Ins', 'Home', 'PgUp'
   ],
   third_row: [
     'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O',
@@ -74,7 +76,7 @@ function getClassForKey(key_label: string): string {
     'Ins': 'key-ins',
     'Del': 'key-del',
     'Up': 'key-up',
-    'Backspace': 'key-backspace',
+    'Back': 'key-backspace',
     'CapsLk': 'key-capslock',
     'Tab': 'key-tab',
     'Enter': 'key-enter',
@@ -366,7 +368,7 @@ const Controls = () => {
   const [isDark, setIsDark] = useState(false);
   const rgb = hexToRgb(color);
   return (
-    <main className='flex flex-col m-auto justify-center mt-[28rem] lg:mt-4 lg:rotate-0'>
+    <main className='flex flex-col m-auto justify-center mt-[28rem] lg:mt-4 '>
       <Keyboard selected_color={color} selected_board_color={boardColor} />
       <div className='flex flex-row mx-auto justify-center mt-4 '>
         <div className='flex flex-col min-w-[14rem]'>
